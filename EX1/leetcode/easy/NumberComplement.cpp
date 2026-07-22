847ec69b334838226cb44bd7cdee7545d22201f4
David Prince J <princed4vid77@gmail.com>
2026-07-22 15:49:19 +0530

    Create NumberComplement.cpp

---
+++ EX1/leetcode/easy/NumberComplement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+
+    long mask = 1;
+        while(mask<=num){
+            mask<<=1;
+        }
+        return num^(mask-1);
+    }
+};
